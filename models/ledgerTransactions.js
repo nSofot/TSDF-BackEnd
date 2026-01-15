57b6d8ed4f47d4453da9aa2f38059973eede7714
Nihal <nihalranathunge@gmail.com>
2026-01-15 17:11:14 +0530

    2026-01-15 ✏️

--- models/ledgerTransactions.js
+++ models/ledgerTransactions.js
@@ -28,7 +28,7 @@ const ledgerTransactionsSchema = new mongoose.Schema({
     },
     description: {
         type: String,
-        required: true
+        default: ""
     },
     isCredit: {
         type: Boolean,
